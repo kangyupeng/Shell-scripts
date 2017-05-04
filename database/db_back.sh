625f57e21daba94ffe53a6bcb4800708a4001c21
kangyupeng <1129004103@qq.com>
2017-05-04 22:28:38 +0800

    mysql  Db backup
    然后使用将此脚本加到 /etc/crontab 定时任务中：
    
    01 5 * * 0 sh /home/mysql/backup.sh
    
    好了，每周日凌晨 5:01 系统就会自动运行 backup.sh 文件备份 MySQL 数据库了。

--- database/db_back.sh
+++ database/db_back.sh
@@ -1,25 +1,48 @@
 #!/bin/sh
-# jian0307@icloud.com
+# 1129004103@qq.com
 
-#
-MYSQL_CNF=/etc/my.cnf
-BACK_CMD=/usr/bin/mysqldump
-BACK_DIR=/data/backup/database
-BACK_FILE=${BACK_DIR}/pkuvr_db_backup.`date +%Y%m%d_%H%M%S`.sql.gz
+#usage:
+#-------------------------------------------------------------------
+# dbname，你需要备份数据库名字。
+# dbname,dbusername,修改为对应数据库用户名和密码。
+# db_backup_name,为导出后的sql文件名字，根据需要自己起名即可。
+#-------------------------------------------------------------------
 
-#
-GZ_CMD=/bin/gzip
 
-#
-DB_HOST=127.0.0.1
-DB_DATABASE=dbname
-DB_USER=user
-DB_PASS=password
+dbname="ceshi_20170501"
+dbusername="-uroot"
+dbpassword="-p123456"
+backup_time=`date +"%Y%m%d%H"`
+db_backup_dir="/data/DataBack"
+db_backup_name="test"
+format=".tar.gz"
 
-#
-cd ${BACK_DIR}
-unalias rm 2> /dev/null
-rm -rf `find . -name '*.sql.gz' -mtime 10` 2> /dev/null
 
-#
-${BACK_CMD} --defaults-extra-file=${MYSQL_CNF} ${DB_DATABASE} | ${GZ_CMD} > ${BACK_FILE} 
+#导出指定数据库
+if [[ -d $db_backup_dir ]]; then
+    /usr/bin/mysqldump $dbusername $dbpassword $dbname > $db_backup_dir/$db_backup_name-$backup_time.sql
+    mkdir $db_backup_dir/log
+    echo "Backup success." $backup_time >> $db_backup_dir/log/dbback.log
+else
+    cd /home;mkdir -p dataBackup/log
+    /usr/local/mysql/bin/mysqldump $dbusername $dbpassword $dbname > $db_backup_dir/$db_backup_name-$backup_time.sql
+    echo "Backup2 success." $backup_time >>  $db_backup_dir/log/dbback.log
+fi
+
+#将备份文件压缩为tar.gz格式
+
+cd $db_backup_dir
+if [[ -f $db_backup_name-$backup_time.sql ]]; then
+    tar -czvf $db_backup_name-$backup_time$format $db_backup_name-$backup_time.sql
+    echo "Compression complete." $backup_time >> $db_backup_dir/log/dbbak.log
+fi
+
+#删除导出的sql文件
+cd $db_backup_dir
+if [[ -f $db_backup_name-$backup_time.sql ]]; then
+    rm -rf $db_backup_name-$backup_time.sql
+    echo "Delete Sql file success." $backup_time >> $db_backup_dir/log/delsql.log
+fi
+
+#清除历史备份仅保留七天
+find $db_backup_dir -type f -mtime +7 | xargs rm -f
